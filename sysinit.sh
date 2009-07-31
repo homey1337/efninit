902ac120e1002f02efc28e13528d77abba3d6f53
Dan McNair <glucnac@gmail.com>
2009-07-30 19:54:21 -0700

    some tweaks to sysinit.sh

--- sysinit.sh
+++ sysinit.sh
@@ -5,16 +5,15 @@
 /sbin/udevd --daemon
 /sbin/udevadm trigger
 /sbin/udevadm settle
-/sbin/fsck -A -T -C -a
-#fsck_check
 /sbin/hwclock --adjust
 /sbin/hwclock --hctosys
+/sbin/fsck -A -T -C -a
+#fsck_check
 /bin/mount -n -o remount,rw /
-#sh /bin/rm -f /etc/mtab*
+/bin/rm -f /etc/mtab
 /bin/cp /proc/mounts /etc/mtab
 /bin/mount -a
-#sh /bin/rm -rf /etc/nologin /etc/shutdownpid /var/lock/* /tmp/* /tmp/.* /forcefsck &>/dev/null
-/bin/find /var/run ! -type d -exec rm -f -- {} ;
+/bin/find /etc/nologin /forcefsck /var/run /var/lock ! -type d -exec /bin/rm -f -- {} ;
 /bin/cp /dev/null /var/run/utmp
 /bin/chmod 0664 /var/run/utmp
 /sbin/ifconfig lo 127.0.0.1 up
